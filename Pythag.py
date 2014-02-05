b3c4d799b6ad3c3ff5a968c0c270a037f354d502
10mmason <10mmason@ashmanorschool.com>
2014-02-05 10:38:39 +0000

    Create Pythag.py

---
+++ Pythag.py
@@ -0,0 +1,13 @@
+def squ(a*a)
+    answer = a*a
+    return (answer)
+    
+def squ2(b*b)
+    answer2 = b*b
+    return (answer2)
+    
+def root(math.sqrt(answer + answer2)
+    answer3 = math.sqrt(answer + answer2)
+    return (answer3)
+print(answer3)
+    
